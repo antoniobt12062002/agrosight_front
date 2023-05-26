166129a64381218169bca99f5943a767688853d8
antoniobt12062002 <antoniotanahashi12@gmail.com>
2023-05-26 18:35:16 -0300

    modified:   src/App.css 	modified:   src/App.js 	modified:   src/components/Dashboard.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Layout } from 'antd';
+import './App.css';
 import Sidebar from './components/Sidebar';
 import Dashboard from './components/Dashboard';
 
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Layout, Row, Col } from 'antd';
-import { UserOutlined, BellOutlined } from '@ant-design/icons';
+import { UserOutlined, BellOutlined, FieldNumberOutlined, FireOutlined, DropboxOutlined, ExperimentOutlined, AlertOutlined, HomeOutlined } from '@ant-design/icons';
 
 const { Header, Content } = Layout;
 
@@ -16,46 +16,68 @@ const Dashboard = () => {
         </div>
       </Header>
       <Content style={{ padding: '24px' }}>
-        <Row gutter={[24, 24]}>
-          <Col span={8}>
-            <div style={squareStyle}>
-              <h3 style={{ ...textStyle, color: '#73af4b' }}>Nitrogênio</h3>
-              <p style={textStyle}>Quantidade: X</p>
-            </div>
-          </Col>
-          <Col span={8}>
-            <div style={squareStyle}>
-              <h3 style={{ ...textStyle, color: '#73af4b' }}>Fósforo</h3>
-              <p style={textStyle}>Quantidade: Y</p>
-            </div>
-          </Col>
-          <Col span={8}>
-            <div style={squareStyle}>
-              <h3 style={{ ...textStyle, color: '#73af4b' }}>Potássio</h3>
-              <p style={textStyle}>Quantidade: Z</p>
-            </div>
-          </Col>
-        </Row>
-        <Row gutter={[24, 24]}>
-          <Col span={8}>
-            <div style={squareStyle}>
-              <h3 style={{ ...textStyle, color: '#73af4b' }}>pH</h3>
-              <p style={textStyle}>Quantidade: V</p>
-            </div>
-          </Col>
-          <Col span={8}>
-            <div style={squareStyle}>
-              <h3 style={{ ...textStyle, color: '#73af4b' }}>Temperatura</h3>
-              <p style={textStyle}>Quantidade: T</p>
-            </div>
-          </Col>
-          <Col span={8}>
-            <div style={{ ...squareStyle, marginBottom: '24px' }}>
-              <h3 style={{ ...textStyle, color: '#73af4b' }}>Umidade</h3>
-              <p style={textStyle}>Quantidade: U</p>
-            </div>
-          </Col>
-        </Row>
+        <div style={{ marginBottom: '24px' }}>
+          <Row gutter={[24, 24]}>
+            <Col span={8}>
+              <div style={squareStyle}>
+                <div style={circleStyle}>
+                  <FieldNumberOutlined style={{ fontSize: '24px', color: '#fff' }} />
+                </div>
+                <h3 style={{ ...textStyle, color: '#73af4b' }}>Nitrogênio</h3>
+                <p style={textStyle}>Quantidade: X</p>
+              </div>
+            </Col>
+            <Col span={8}>
+              <div style={squareStyle}>
+                <div style={circleStyle}>
+                  <DropboxOutlined style={{ fontSize: '24px', color: '#fff' }} />
+                </div>
+                <h3 style={{ ...textStyle, color: '#73af4b' }}>Fósforo</h3>
+                <p style={textStyle}>Quantidade: Y</p>
+              </div>
+            </Col>
+            <Col span={8}>
+              <div style={squareStyle}>
+                <div style={circleStyle}>
+                  <FireOutlined style={{ fontSize: '24px', color: '#fff' }} />
+                </div>
+                <h3 style={{ ...textStyle, color: '#73af4b' }}>Potássio</h3>
+                <p style={textStyle}>Quantidade: Z</p>
+              </div>
+            </Col>
+          </Row>
+        </div>
+        <div style={{ marginBottom: '24px' }}>
+          <Row gutter={[24, 24]}>
+            <Col span={8}>
+              <div style={squareStyle}>
+                <div style={circleStyle}>
+                  <ExperimentOutlined style={{ fontSize: '24px', color: '#fff' }} />
+                </div>
+                <h3 style={{ ...textStyle, color: '#73af4b' }}>pH</h3>
+                <p style={textStyle}>Quantidade: V</p>
+              </div>
+            </Col>
+            <Col span={8}>
+              <div style={squareStyle}>
+                <div style={circleStyle}>
+                  <AlertOutlined style={{ fontSize: '24px', color: '#fff' }} />
+                </div>
+                <h3 style={{ ...textStyle, color: '#73af4b' }}>Temperatura</h3>
+                <p style={textStyle}>Quantidade: T</p>
+              </div>
+            </Col>
+            <Col span={8}>
+              <div style={squareStyle}>
+                <div style={circleStyle}>
+                  <HomeOutlined style={{ fontSize: '24px', color: '#fff' }} />
+                </div>
+                <h3 style={{ ...textStyle, color: '#73af4b' }}>Umidade</h3>
+                <p style={textStyle}>Quantidade: U</p>
+              </div>
+            </Col>
+          </Row>
+        </div>
       </Content>
     </Layout>
   );
@@ -70,6 +92,17 @@ const squareStyle = {
   flexDirection: 'column',
   justifyContent: 'center',
   alignItems: 'center',
+};
+
+const circleStyle = {
+  width: '48px',
+  height: '48px',
+  background: '#73af4b',
+  borderRadius: '50%',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  marginBottom: '16px',
 };
 
 const textStyle = {
